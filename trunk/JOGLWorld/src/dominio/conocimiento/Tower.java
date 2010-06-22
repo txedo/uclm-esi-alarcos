53d49e82670b02757de502bf64e854c7eb2eb432
lopez.josedomingo <lopez.josedomingo@c9671f85-e379-49f0-1933-c7bbfdc84eab>
2010-06-22 09:53:08 +0000

    Modifacada la clase Tower para que las torres se dibujen en vertical, tomando como base el plano ZX y como altura el eje Y

--- trunk/JOGLWorld/src/dominio/conocimiento/Tower.java
+++ trunk/JOGLWorld/src/dominio/conocimiento/Tower.java
@@ -9,16 +9,16 @@ public class Tower {
 	 * La altura se extiende a lo largo del eje Y y viene dada por la variable height
 	 */
 	private float origin_x;
-	private float origin_y;
+	private float origin_z;
 	private float width;
 	private float height;
 	private Color color;
 
-	public Tower(float origin_x, float origin_y, float width, float height,
+	public Tower(float origin_x, float origin_z, float width, float height,
 			Color color) {
 		super();
 		this.origin_x = origin_x;
-		this.origin_y = origin_y;
+		this.origin_z = origin_z;
 		this.width = width;
 		this.height = height;
 		this.color = color;
@@ -28,36 +28,36 @@ public class Tower {
 		// Aplicamos el mismo color a todos los vértices
 		gl.glColor4fv(color.getColorFB());
 		gl.glBegin(GL.GL_QUADS);
-			// Base
-			gl.glVertex3f(origin_x, origin_y, 0);
-			gl.glVertex3f(origin_x + width, origin_y, 0);
-			gl.glVertex3f(origin_x + width, origin_y + width, 0);
-			gl.glVertex3f(origin_x, origin_y + width, 0);
+			// Base		
+			gl.glVertex3f(origin_x, 0, origin_z);
+			gl.glVertex3f(origin_x + width, 0, origin_z);
+			gl.glVertex3f(origin_x + width, 0, origin_z + width);
+			gl.glVertex3f(origin_x, 0, origin_z + width);
 			// Frente
-			gl.glVertex3f(origin_x + width, origin_y, 0);
-			gl.glVertex3f(origin_x + width, origin_y + width, 0);
-			gl.glVertex3f(origin_x + width, origin_y + width, height);
-			gl.glVertex3f(origin_x + width, origin_y, height);
+			gl.glVertex3f(origin_x + width, 0, origin_z);
+			gl.glVertex3f(origin_x + width, 0, origin_z + width);
+			gl.glVertex3f(origin_x + width, height, origin_z + width);
+			gl.glVertex3f(origin_x + width, height, origin_z);
 //			// Lado derecho
-			gl.glVertex3f(origin_x + width, origin_y + width, 0);
-			gl.glVertex3f(origin_x, origin_y + width, 0);
-			gl.glVertex3f(origin_x, origin_y + width, height);
-			gl.glVertex3f(origin_x + width, origin_y + width, height);
+			gl.glVertex3f(origin_x + width, 0, origin_z + width);
+			gl.glVertex3f(origin_x, 0, origin_z + width);
+			gl.glVertex3f(origin_x, height, origin_z + width);
+			gl.glVertex3f(origin_x + width, height, origin_z + width);
 //			// Espalda
-			gl.glVertex3f(origin_x, origin_y, 0);
-			gl.glVertex3f(origin_x, origin_y + width, 0);
-			gl.glVertex3f(origin_x, origin_y + width, height);
-			gl.glVertex3f(origin_x, origin_y, height);
+			gl.glVertex3f(origin_x, 0, origin_z);
+			gl.glVertex3f(origin_x, 0, origin_z + width);
+			gl.glVertex3f(origin_x, height, origin_z + width);
+			gl.glVertex3f(origin_x, height, origin_z);
 //			// Lado izquierdo
-			gl.glVertex3f(origin_x + width, origin_y, 0);
-			gl.glVertex3f(origin_x, origin_y, 0);
-			gl.glVertex3f(origin_x, origin_y, height);
-			gl.glVertex3f(origin_x + width, origin_y, height);
+			gl.glVertex3f(origin_x + width, 0, origin_z);
+			gl.glVertex3f(origin_x, 0, origin_z);
+			gl.glVertex3f(origin_x, height, origin_z);
+			gl.glVertex3f(origin_x + width, height, origin_z);
 //			// Planta (igual que la base pero con eje Z = height
-			gl.glVertex3f(origin_x, origin_y, height);
-			gl.glVertex3f(origin_x + width, origin_y, height);
-			gl.glVertex3f(origin_x + width, origin_y + width, height);
-			gl.glVertex3f(origin_x, origin_y + width, height);
+			gl.glVertex3f(origin_x, height, origin_z);
+			gl.glVertex3f(origin_x + width, height, origin_z);
+			gl.glVertex3f(origin_x + width, height, origin_z + width);
+			gl.glVertex3f(origin_x, height, origin_z + width);
 		gl.glEnd();
 	}
 
