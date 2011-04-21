31fee3ae167be6df22cb0f2be966dce0dd158c88
lopez.josedomingo <lopez.josedomingo@c9671f85-e379-49f0-1933-c7bbfdc84eab>
2011-04-21 08:25:25 +0000

    * Añadida comunicación applet -> browser

--- trunk/desglosa/src/main/java/presentation/AppletMain.java
+++ trunk/desglosa/src/main/java/presentation/AppletMain.java
@@ -2,6 +2,8 @@ package presentation;
 
 import java.applet.*;
 import java.awt.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.media.opengl.GLCapabilities;
 import javax.media.opengl.GLProfile;
@@ -9,9 +11,13 @@ import javax.media.opengl.awt.GLCanvas;
 import com.jogamp.opengl.util.FPSAnimator;
 import javax.media.opengl.GLAnimatorControl;
 
+import model.IObserverUI;
+import model.NotifyUIManager;
+import model.business.knowledge.Factory;
 import model.gl.GLDrawer;
+import model.knowledge.Vector2f;
 
-public class AppletMain extends Applet {
+public class AppletMain extends Applet implements IObserverUI {
 	private GLAnimatorControl animator;
 
 	@Override
@@ -35,6 +41,8 @@ public class AppletMain extends Applet {
 		canvas.setSize(getSize());
 		add(canvas, BorderLayout.CENTER);
 		animator = new FPSAnimator(canvas, 60);
+		
+		NotifyUIManager.attach(this);
 		System.err.println("GearsApplet: init() - end");
 
 	}
@@ -55,6 +63,102 @@ public class AppletMain extends Applet {
 
 	public void destroy() {
 		System.err.println("GearsApplet: destroy() - X");
+	}
+
+	@Override
+	public void updateCompanyList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateFactoryList(int idCompany) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateMapList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateProjectList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateClickedWorldCoords(Vector2f coordinates) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selectFactoryByLocation(int idLocation) {
+		this.handleEvent(EOperationCodes.FactorySelectionByLocation, idLocation);
+	}
+
+	@Override
+	public void selectProject(int id) {
+		this.handleEvent(EOperationCodes.ProjectSelection, id);
+	}
+
+	@Override
+	public void selectFactory(int id) {
+		this.handleEvent(EOperationCodes.FactorySelection, id);
+	}
+
+	@Override
+	public void selectFactory(Factory factory) {
+		this.handleEvent(EOperationCodes.FactorySelection, factory);
+	}
+
+	@Override
+	public void selectTower(int id) {
+		this.handleEvent(EOperationCodes.TowerSelection, id);
+	}
+	
+	private void handleEvent (EOperationCodes opCode, Object ob) {
+		try {
+			switch (opCode) {
+			case TowerSelection:
+				if (ob instanceof Integer) {
+					int id = (Integer)ob;
+					getAppletContext().showDocument(new URL("javascript:selectTower(" + id + ")"));
+				}
+				break;
+			case FactorySelection:
+				if (ob instanceof Integer) {
+					int id = (Integer)ob;
+					getAppletContext().showDocument(new URL("javascript:selectFactory(" + id + ")"));
+				} else if (ob instanceof Factory) {
+					Factory f = (Factory)ob;
+					getAppletContext().showDocument(new URL("javascript:selectFactory(" + f.getId() + ")"));
+				}
+				break;
+			case ProjectSelection:
+				if (ob instanceof Integer) {
+					int id = (Integer)ob;
+					getAppletContext().showDocument(new URL("javascript:selectProject(" + id + ")"));
+				}
+				break;
+			case FactorySelectionByLocation:
+				if (ob instanceof Integer) {
+					int id = (Integer)ob;
+					getAppletContext().showDocument(new URL("javascript:selectFactoryByLocation(" + id + ")"));
+				}
+				break;
+			default:
+				getAppletContext().showDocument(new URL("javascript:alert(\'Undefined operation\')"));
+				break;
+			}
+		} catch (MalformedURLException e) {
+			try {
+				getAppletContext().showDocument(new URL("javascript:alert(\'" + e.toString() + "\')"));
+			} catch (MalformedURLException e1) {}
+		}
 	}
 
 }
