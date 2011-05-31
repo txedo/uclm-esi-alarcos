752327602edc103f7d91cae1137b52fa3514f5ae
Txedo <txedo@users.noreply.github.com>
2011-05-31 19:03:09 +0000

    Esta versión se corresponde con el motor gráfico con la interfaz gráfica y la lógica de negocio embebida. Se crea este TAG porque la lógica de negocio se moverá a la aplicación web y el motor gráfico sufrirá grandes cambios.

--- desglosa-web/src/main/java/es/uclm/inf_cr/alarcos/desglosa_web/actions/GenericActionInterface.java
+++
@@ -1,8 +0,0 @@
-package es.uclm.inf_cr.alarcos.desglosa_web.actions;
-
-public interface GenericActionInterface {
-	public String save() throws Exception;
-	public String edit() throws Exception;
-	public String delete() throws Exception;
-	public String get() throws Exception;
-}
--- desglosa-web/src/main/java/es/uclm/inf_cr/alarcos/desglosa_web/actions/UserAction.java
+++
@@ -1,5 +0,0 @@
-package es.uclm.inf_cr.alarcos.desglosa_web.actions;
-
-public class UserAction {
-
-}
