4b18d0de0451bf72560d57c5b8117453f32e6a19
lopez.josedomingo <lopez.josedomingo@c9671f85-e379-49f0-1933-c7bbfdc84eab>
2011-07-07 12:15:25 +0000

    * Se ha añadido el DAO de la clase Market. * Se está haciendo una integración más directa entre el motor gráfico y la aplicación web para no tener tanta dependencia de JS

--- trunk/desglosa-web/src/main/java/es/uclm/inf_cr/alarcos/desglosa_web/model/Market.java
+++ trunk/desglosa-web/src/main/java/es/uclm/inf_cr/alarcos/desglosa_web/model/Market.java
@@ -5,10 +5,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="markets")
+@NamedQueries ({
+    @NamedQuery(
+        	name = "findMostRepresentativeMarketByFactoryId",
+        	query = "select m from Factory f, Subproject sp, Project p, Market m where f.id = :id and f.id = sp.factory.id and sp.project.id = p.id and p.market.id = m.id group by m.name order by count(p.id) desc limit 1"
+        )
+})
 public class Market {
 	private int id;
 	private String name;
