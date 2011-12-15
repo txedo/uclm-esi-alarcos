875450fa49ef8158e8b0b2948f077be15b0912f5
lopez.josedomingo <lopez.josedomingo@c9671f85-e379-49f0-1933-c7bbfdc84eab>
2011-12-15 13:21:05 +0000

    * Pruebas

--- trunk/desglosa-web/src/main/java/es/uclm/inf_cr/alarcos/desglosa_web/control/CompanyManager.java
+++ trunk/desglosa-web/src/main/java/es/uclm/inf_cr/alarcos/desglosa_web/control/CompanyManager.java
@@ -12,13 +12,38 @@ public class CompanyManager {
     private CompanyManager() {
     }
     
-    public static Company checkCompanyExists(int id) throws CompanyNotFoundException {
-        // Check if the company id exists
-        return CompanyManager.getCompany(id);
+    public static boolean checkCompanyExists(int id) {
+        boolean bExists = true;
+        try {
+            CompanyManager.getCompany(id);
+            bExists = true;
+        } catch (CompanyNotFoundException e) {
+            bExists = false;
+        }
+        return bExists;
+    }
+    
+    public static boolean checkCompanyExists(String name) {
+        boolean bExists = true;
+        try {
+            CompanyManager.getCompany(name);
+            bExists = true;
+        } catch (CompanyNotFoundException e) {
+            bExists = false;
+        }
+        return bExists;
+    }
+    
+    public static void saveCompany(Company c) {
+        companyDao.saveCompany(c);
     }
     
     public static Company getCompany(int id) throws CompanyNotFoundException {
         return companyDao.getCompany(id);
+    }
+    
+    public static Company getCompany(String name) throws CompanyNotFoundException {
+        return companyDao.getCompany(name);
     }
     
     public static List<Company> getAllCompanies() {
--- trunk/desglosa-web/src/test/java/es/uclm/inf_cr/alarcos/desglosa_web/actions/CompanyActionTest.java
+++ trunk/desglosa-web/src/test/java/es/uclm/inf_cr/alarcos/desglosa_web/actions/CompanyActionTest.java
@@ -4,22 +4,27 @@ import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 
 import com.opensymphony.xwork2.Action;
 
-import es.uclm.inf_cr.alarcos.desglosa_web.dao.CompanyDAO;
+public class CompanyActionTest extends
+        AbstractDependencyInjectionSpringContextTests {
 
-public class CompanyActionTest extends AbstractDependencyInjectionSpringContextTests {
+    @Override
+    protected String[] getConfigLocations() {
+        return new String[] { "classpath:applicationContext-resources.xml",
+                "classpath:applicationContext-dao.xml",
+                "classpath:applicationContext.xml" };
+    }
+    
+    public void testShowCompany() {
+        CompanyAction action = new CompanyAction();
+        
+    }
 
-	@Override
-	protected String[] getConfigLocations() {
-		return new String[] { 	"classpath:applicationContext-resources.xml",
-								"classpath:applicationContext-dao.xml",
-								"classpath:applicationContext.xml"};
-	}
+    public void testListCompaniesAction() {
+        CompanyAction action = new CompanyAction();
+        //action.setCompanyDao((CompanyDAO) applicationContext.getBean("companyDao"));
+        String result = action.execute();
+        
+        assertEquals(Action.SUCCESS, result);
+    }
 
-	public void testListCompaniesAction() throws Exception {
-		CompanyAction action = new CompanyAction();
-		action.setCompanyDao((CompanyDAO) applicationContext.getBean("companyDao"));
-		String result = action.execute();
-		assertEquals(Action.SUCCESS, result);
-	}
-	
 }
